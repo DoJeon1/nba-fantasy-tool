dfa5c234cb6b2c3c69498132ddfd06249499f213
DoJeon1 <donghwan.dj@gmail.com>
2021-04-23 00:35:22 -0600

    currently extracts the job title and company where the position is hiring from

--- scraper.py
+++ scraper.py
@@ -1,8 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.linkedin.com/jobs/search/?geoId=101174742&keywords=software%20intern&location=Canada"
+URL = 'https://ca.indeed.com/jobs?q=software+developer&l=canada'
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
+html = BeautifulSoup(page.content, 'html.parser')
+results = html.find(id='resultsCol')
+jobPostings = results.find_all(class_='jobsearch-SerpJobCard')
+
+for posting in jobPostings:
+    title = posting.find('h2', class_='title')
+    company = posting.find('span', class_='company')
+    #location = posting.find('div', class_='recJobLoc')
+    print(title.text)
+    print(company.text)
+    #print(location.text)
+    print()
