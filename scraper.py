41936135876a08567c5c00a856e39b451c9a2d38
DoJeon1 <donghwan.dj@gmail.com>
2021-04-25 22:12:55 -0600

    scraper file now writes cleaned data to txt file

--- scraper.py
+++ scraper.py
@@ -8,12 +8,22 @@ html = BeautifulSoup(page.content, 'html.parser')
 results = html.find(id='resultsCol')
 jobPostings = results.find_all(class_='jobsearch-SerpJobCard')
 
+jobFile = open('job-data.txt', 'w')
+
 for posting in jobPostings:
     title = posting.find('a', class_='jobtitle')
     company = posting.find('span', class_='company')
-    #location = posting.find('div', class_='location accessible-contrast-color-location')
     location = posting.find(class_='location')
+    
+    jobFile.write(title.text.strip() + "\n")
+    jobFile.write(company.text.strip() + "\n")
+    jobFile.write(location.text.strip() + "\n")
+    jobFile.write("\n")
+
+"""
+Printing to the command line 
     print(title.text.strip())
     print(company.text.strip())
     print(location.text.strip())
     print()
+ """
