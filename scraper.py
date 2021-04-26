b23d15bb48f6edda8cec3ba5ad44d4ee435fdb8b
DoJeon1 <donghwan.dj@gmail.com>
2021-04-25 22:28:46 -0600

    changed file to csv file, added new import

--- scraper.py
+++ scraper.py
@@ -8,23 +8,25 @@ html = BeautifulSoup(page.content, 'html.parser')
 results = html.find(id='resultsCol')
 jobPostings = results.find_all(class_='jobsearch-SerpJobCard')
 
-jobFile = open('job-data.txt', 'w')
+#jobFile = open('job-data.txt', 'w')
+import csv 
+with open('job-data.csv', 'w', newline='') as file:
+    jobFile = csv.writer(file)
 
-for posting in jobPostings:
-    title = posting.find('a', class_='jobtitle')
-    company = posting.find('span', class_='company')
-    location = posting.find(class_='location')
-    
-    jobFile.write(title.text.strip() + "\n")
-    jobFile.write(company.text.strip() + "\n")
-    jobFile.write(location.text.strip() + "\n")
-    jobFile.write("\n")
-    """
-    Printing to the command line ...
-    print(title.text.strip())
-    print(company.text.strip())
-    print(location.text.strip())
-    print()
-    """
+    for posting in jobPostings:
+        title = posting.find('a', class_='jobtitle')
+        company = posting.find('span', class_='company')
+        location = posting.find(class_='location')
+        
+        jobFile.writerow(title.text.strip())
+        jobFile.writerow(company.text.strip())
+        jobFile.writerow(location.text.strip())
+        jobFile.writerow('\n')
 
-jobFile.close()
+        """
+        Writing to a text file
+        jobFile.write(title.text.strip() + "\n")
+        jobFile.write(company.text.strip() + "\n")
+        jobFile.write(location.text.strip() + "\n")
+        jobFile.write("\n")
+        """
